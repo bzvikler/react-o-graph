25fc2bacafbe7c63dba79becd1ab0ad806f44c59
Ben Zvikler <bzvikler@gmail.com>
2019-11-18 21:33:24 -0800

    Adds analysis-api TODOs

--- directory-service/copy-files/analysis-api.js
+++ directory-service/copy-files/analysis-api.js
@@ -1,14 +1,41 @@
-const onUpdate = () => {
+/**
+ * Makes network call to graph-ui server with the given react component details
+ *
+ * @param {Object} node of shape { id: string, name: string, props: Object, state: Object }
+ *
+ * @returns {void}
+ */
+const onUpdate = (node) => {
   // TODO: network calls
   console.log('ON UPDATE');
 };
 
-const onMount = () => {
+/**
+ * Makes network call to graph-ui server with the given react component details
+ *
+ * @param {Object} node of shape { id: string, name: string, props: Object, state: Object }
+ *
+ * @returns {void}
+ */
+const onMount = (node) => {
+  // TODO: network calls
   console.log('ON MOUNT');
-  fetch('http://localhost:5000/addRandomNode');
 };
+
+/**
+ * Makes network call to graph-ui server with the given react component details
+ *
+ * @param {Object} node of shape { id: string, name: string, props: Object, state: Object }
+ *
+ * @returns {void}
+ */
+const onUnmount = (node) => {
+  // TODO: network calls
+  console.log('ON UNMOUNT');
+}
 
 export default {
   onUpdate,
   onMount,
+  onUnmount,
 };
