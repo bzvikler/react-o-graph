b3e28c7365aa1f9921009484297451641d686131
chlorophyll95 <chad.loro.cl@gmail.com>
2019-11-20 22:53:56 -0800

    Added mock doughnuts to NodeViewer

--- graph-ui/client/src/NodeViewer.js
+++ graph-ui/client/src/NodeViewer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Doughnut } from "react-chartjs-2";
 
 // View expanded info showing metadata and additional visualizations
 // for each individual component
@@ -6,21 +7,65 @@ import React from 'react';
 
 // CHAD - THIS IS FOR YOU
 
+const data = {
+    labels: [
+        "red",
+        "green",
+        "yellow"
+    ],
+    datasets: [{
+        data: [300, 50, 100],
+        backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+        ],
+        hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56'
+        ]
+    }]
+};
+
 export default class NodeViewer extends React.Component {
     constructor(props) {
         super(props);
     }
-    
+
     render() {
         // this is just a mock display for now
         return (
             <div className="info">
-                <b>Information</b><br/>
-                Component: {this.props.node.name} <br/>
-                Id: {this.props.node.id} <br />
-                Props: {JSON.stringify(this.props.node.props)} <br />
-                State: {JSON.stringify(this.props.node.state)} <br />
+                <Doughnut
+                    data={data}
+                />
+                <Doughnut
+                    data={data}
+                />
+                <Doughnut
+                    data={data}
+                />
+                <Doughnut
+                    data={data}
+                />
+                <Doughnut
+                    data={data}
+                />
+                <Doughnut
+                    data={data}
+                />
+                <Doughnut
+                    data={data}
+                />
             </div>
         );
     }
 }
+
+
+// <b>Information</b><br/>
+// Component: {this.props.node.name} <br/>
+// Id: {this.props.node.id} <br />
+// Props: {JSON.stringify(this.props.node.props)} <br />
+// State: {JSON.stringify(this.props.node.state)} <br />
