582a0b37a868d7782591b5be8739db4dd3a4c7f0
Ben Zvikler <bzvikler@gmail.com>
2019-11-22 11:01:15 -0800

    Adds fix for code injector thinking index.js is a component

--- directory-service/filters/index.js
+++ directory-service/filters/index.js
@@ -1,5 +1,6 @@
 export const isComponent = fileString => (
-  fileString.indexOf('import React') !== -1 && fileString.indexOf('Component') !== -1
+  fileString.indexOf('class') !== -1 && fileString.indexOf('extends') !== -1 && fileString.indexOf('Component') !== -1 &&
+  fileString.indexOf('import React') !== -1
 );
 export const isRenderLine = line => line.indexOf('render()') !== -1;
 export const isJsOrJsx = fileName => fileName.indexOf('.js') !== -1;
