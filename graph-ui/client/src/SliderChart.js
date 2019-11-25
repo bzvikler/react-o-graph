68d5781c49012fd4b8b3b6325a518044a2a7e24d
Susan Chen <susanszc@gmail.com>
2019-11-24 22:27:31 -0800

    slider steps change base on data length

--- graph-ui/client/src/SliderChart.js
+++ graph-ui/client/src/SliderChart.js
@@ -2,12 +2,21 @@ import React from "react";
 import Slider from "rc-slider";
 import ReactJson from "react-json-view";
 
+const sliderMap = {
+    5: { 0: "Earlier", 1: "", 2: "", 3: "", 4: "Latest" },
+    4: { 0: "Earliest", 1: "", 2: "", 3: "Latest" },
+    3: { 0: "Earliest", 1: "", 2: "Latest" },
+    2: { 0: "Earliest", 1: "Latest" },
+    1: { 0: "Latest"},
+    0: { 0: "Latest"},
+}
+
 export default class SliderChart extends React.Component {
     constructor(props) {
         super(props);
         
         this.state = {
-            currentVal: 100
+            currentVal: props.data.length < 2? 1 : props.data.length - 1
         }
 
         this.changeHistory = this.changeHistory.bind(this);
@@ -20,27 +29,23 @@ export default class SliderChart extends React.Component {
     componentDidUpdate(prevProps) {
         // reset slider when new component selected
         if (this.props.id != prevProps.id) {
-            this.setState({currentVal: 100})
+            this.setState({currentVal: this.props.data.length})
         }
     }
 
     render() {
-        let idx = this.state.currentVal / 25;
-        let data = this.props.data;
-        while (data.length < 5) {
-            data.unshift({});
-        }
-        let currentDisplay = data[idx] == null ? {} : data[idx];
+        let currentDisplay = this.props.data[this.state.currentVal];
+        if (!currentDisplay || currentDisplay == null)
+            currentDisplay = {};
         return (
             <div>
-                <p className="slider-title">{this.props.name}</p>
+                <p className="slider-title">{this.props.name} {this.props.data.length}</p>
                 <div className="slider-chart">
                     <Slider 
                         min={0}
-                        defaultValue={100} 
+                        max={this.props.data.length < 2 ? 0 : this.props.data.length - 1}
                         value={this.state.currentVal}
-                        marks={{ 0: "Earliest", 25: "", 50: "", 75: "", 100: "Latest" }} 
-                        step={null}
+                        marks={sliderMap[this.props.data.length]} 
                         onChange={this.changeHistory}
                     />
                 </div>
